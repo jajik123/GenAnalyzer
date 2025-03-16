27497f0d3a63009bffae13f1d74078946bc1d579
YasminFazla <fazla.yasmin@outlook.com>
2025-03-16 13:33:31 +0100

    Delete app directory

--- app/config.py
+++
@@ -1,9 +0,0 @@
-# this file includes class that stores configurations like the secret key
-
-import os
-
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    # the secret key is generated randomly, it must be created if I want to used objects like session
-    SESSION_TYPE = "filesystem"
